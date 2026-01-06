66113ec8c241ed60056da604c8862e0ad297ccb9
KPorus <designerfardin2021@gmail.com>
2026-01-06 17:00:39 +0600

    add git actions

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+       base: '/nova-news-portal-v2/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
